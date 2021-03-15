8805143dfff30a520b8cf6fe963a28845e3c50b4
VoVuHy <vovuhy163@gmail.com>
2021-03-15 19:34:18 +0700

    a
    a

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,15 +21,15 @@ class UserFactory extends Factory
      * @return array
      */
     public function definition()
-    {
-        return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ];
-    }
+{
+    return [
+        'first_name' => $this->faker->name,
+        'last_name' => $this->faker->name,
+        'phone' => $this->faker->phoneNumber,
+        'email' => $this->faker->unique()->safeEmail,
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+    ];
+}
 
     /**
      * Indicate that the model's email address should be unverified.
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-    }
+{
+    // \App\Models\User::factory(10)->create();
+    $this->call(UserSeeder::class);
+}
 }
